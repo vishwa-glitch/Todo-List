fe3fd7c604f80175648b1f3f016db40066a38545
Vishwa Karikal <153448025+vishwa-glitch@users.noreply.github.com>
2025-01-08 09:52:55 +0530

    Update settings.py

--- project01/settings.py
+++ project01/settings.py
@@ -19,13 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h==p(mz%mm9tf30drro6np29$z&!)4(ah5#%t1_w%u3wecb!@_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [todo-list-hbqg.onrender.com
 ]
 
 
